09e43a738bffbc50dbfb58d1c3a1ce9679a8b2f4
ainolpipeline <ainol.fhatah@pipeline-network.com>
2021-01-05 14:41:33 +0800

    Role seeding completed with routing

--- database/seeds/RolesTableSeeder.php
+++ database/seeds/RolesTableSeeder.php
@@ -13,24 +13,72 @@ class RolesTableSeeder extends Seeder
     {
         DB::table('roles')->insert([
             'id' => 1,
-            'name' => 'Admin',
-            'description' => 'This is the administration role',
+            'name' => 'Pentadbir Sistem',
+            'description' => 'This is the Pentadbir Sistem role',
             'created_at' => now(),
             'updated_at' => now()
         ]);
 
         DB::table('roles')->insert([
             'id' => 2,
-            'name' => 'Creator',
-            'description' => 'This is the creator role',
+            'name' => 'Penyelia',
+            'description' => 'This is the Penyelia role',
             'created_at' => now(),
             'updated_at' => now()
         ]);
 
         DB::table('roles')->insert([
             'id' => 3,
-            'name' => 'Member',
-            'description' => 'This is the member role',
+            'name' => 'Datuk Bandar',
+            'description' => 'This is the Datuk Bandar role',
+            'created_at' => now(),
+            'updated_at' => now()
+        ]);
+
+        DB::table('roles')->insert([
+            'id' => 4,
+            'name' => 'Ketua Bahagian',
+            'description' => 'This is the Ketua Bahagian role',
+            'created_at' => now(),
+            'updated_at' => now()
+        ]);
+
+        DB::table('roles')->insert([
+            'id' => 5,
+            'name' => 'Ketua Jabatan',
+            'description' => 'This is the Ketua Jabatan role',
+            'created_at' => now(),
+            'updated_at' => now()
+        ]);
+
+        DB::table('roles')->insert([
+            'id' => 6,
+            'name' => 'Kerani Semakan',
+            'description' => 'This is the Kerani Semakan role',
+            'created_at' => now(),
+            'updated_at' => now()
+        ]);
+
+        DB::table('roles')->insert([
+            'id' => 7,
+            'name' => 'Kerani Pemeriksaan',
+            'description' => 'This is the Kerani Pemeriksaan role',
+            'created_at' => now(),
+            'updated_at' => now()
+        ]);
+
+        DB::table('roles')->insert([
+            'id' => 8,
+            'name' => 'Kakitangan',
+            'description' => 'This is the Kakitangan role',
+            'created_at' => now(),
+            'updated_at' => now()
+        ]);
+
+        DB::table('roles')->insert([
+            'id' => 9,
+            'name' => 'Pelulus Pindaan',
+            'description' => 'This is the Pelulus Pindaan role',
             'created_at' => now(),
             'updated_at' => now()
         ]);
