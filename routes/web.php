a1dc0cfdfa63a24defeeaace53d577f1a787e839
nikhanif24 <75105543+nikhanif24@users.noreply.github.com>
2021-01-05 13:10:49 +0800

    bantuancontroller for datuk bandar

--- routes/web.php
+++ routes/web.php
@@ -62,6 +62,44 @@ Route::group([
 	Route::get('{page}', ['as' => 'page.index', 'uses' => 'PageController@index']);
 });
 
+Route::group([
+	'prefix' => 'penyelia',
+	'middleware' => [
+	'auth',
+	'role:2'
+	]], function () {
+	Route::resource('/dashboard','kakitangan\dashboardController',['except' => ['show','destroy']]);
+	Route::resource('/category', 'CategoryController', ['except' => ['show']]);
+	Route::resource('/tag', 'TagController', ['except' => ['show']]);
+	Route::resource('/item', 'ItemController', ['except' => ['show']]);
+	Route::resource('/role', 'RoleController', ['except' => ['show', 'destroy']]);
+	Route::resource('/user', 'UserController', ['except' => ['show']]);
+	Route::resource('/semakan','kakitangan\semakanController',['except' => ['show','destroy']]);
+	Route::resource('/bantuan','kakitangan\bantuanController',['except' => ['show','destroy']]);
+	Route::resource('/tuntutan','kakitangan\tuntutanController',['except' => ['show','destroy']]);
+	Route::resource('/laporan','kakitangan\laporanController',['except' => ['show','destroy']]);
+	Route::resource('/permohonan-baru','kakitangan\permohonanController',['except' => ['show','destroy']]);
+	Route::group(['prefix' => 'permohonan-baru'], function () {
+		Route::get('/{user_id}', [
+			'uses' => 'kakitangan\permohonanController@show',
+			'as'   => 'permohonanbaru.show',
+		]);
+	});
+	Route::resource('penyelia-dashboard','penyelia\penyeliaController',['except' => ['show','destroy']]);
+	Route::resource('penyelia-semakan','penyelia\semakanController',['except' => ['show','destroy']]);
+	Route::resource('penyelia-laporan','penyelia\laporanController',['except' => ['show','destroy']]);
+	Route::resource('penyelia-bantuan','penyelia\bantuanController',['except' => ['show','destroy']]);
+
+	Route::get('profile', ['as' => 'profile.edit', 'uses' => 'ProfileController@edit']);
+	Route::get('profile/{link}',function(){
+		return view('profile.index',['link'=>$link]);
+	});
+	Route::put('profile', ['as' => 'profile.update', 'uses' => 'ProfileController@update']);
+	Route::put('profile/password', ['as' => 'profile.password', 'uses' => 'ProfileController@password']);
+
+	Route::get('{page}', ['as' => 'page.index', 'uses' => 'PageController@index']);
+});
+
 Route::group(
 	[
 		'prefix' => 'datuk-bandar',
@@ -72,6 +110,7 @@ Route::group(
 					], 
 					function () {
 	Route::resource('/dashboard','datuk_bandar\dashboardController');
+	Route::resource('/bantuan','datuk_bandar\bantuanController');
 
 });
 
