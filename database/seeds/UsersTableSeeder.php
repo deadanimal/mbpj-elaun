31422a716c90edc16835deb9704f2e12566c444d
nikhanif24 <75105543+nikhanif24@users.noreply.github.com>
2021-01-07 15:35:48 +0800

    routing and seeding

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -14,23 +14,65 @@ class UsersTableSeeder extends Seeder
     {
         factory(App\User::class)->create([
             'id' => 1,
-            'name' => 'Admin',
+            'name' => 'Pentadbir Sistem',
             'email' => 'admin@argon.com',
             'role_id' => 1,
         ]);
 
         factory(App\User::class)->create([
             'id' => 2,
-            'name' => 'Creator',
-            'email' => 'creator@argon.com',
+            'name' => 'Penyelia',
+            'email' => 'py@argon.com',
             'role_id' => 2,
         ]);
 
         factory(App\User::class)->create([
             'id' => 3,
-            'name' => 'Member',
-            'email' => 'member@argon.com',
+            'name' => 'Datuk Bandar',
+            'email' => 'db@argon.com',
             'role_id' => 3,
+        ]);
+
+        factory(App\User::class)->create([
+            'id' => 4,
+            'name' => 'Ketua Bahagian',
+            'email' => 'kb@argon.com',
+            'role_id' => 4,
+        ]);
+
+        factory(App\User::class)->create([
+            'id' => 5,
+            'name' => 'Ketua Jabatan',
+            'email' => 'kj@argon.com',
+            'role_id' => 5,
+        ]);
+
+        factory(App\User::class)->create([
+            'id' => 6,
+            'name' => 'Kerani Semakan',
+            'email' => 'ks@argon.com',
+            'role_id' => 6,
+        ]);
+
+        factory(App\User::class)->create([
+            'id' => 7,
+            'name' => 'Kerani Pemeriksa',
+            'email' => 'kp@argon.com',
+            'role_id' => 7,
+        ]);
+
+        factory(App\User::class)->create([
+            'id' => 8,
+            'name' => 'Kakitangan',
+            'email' => 'kt@argon.com',
+            'role_id' => 8,
+        ]);
+
+        factory(App\User::class)->create([
+            'id' => 9,
+            'name' => 'Pelulus Pindaan',
+            'email' => 'pp@argon.com',
+            'role_id' => 9,
         ]);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -252,6 +252,40 @@ Route::group([
 	Route::get('{page}', ['as' => 'page.index', 'uses' => 'PageController@index']);
 });
 
+Route::group([
+	'prefix' => 'kakitangan',
+	'middleware' => [
+	'auth',
+	'role:8'
+	]], function () {
+	Route::resource('/dashboard','kakitangan\dashboardController',['except' => ['show','destroy']]);
+	Route::resource('/category', 'CategoryController', ['except' => ['show']]);
+	Route::resource('/tag', 'TagController', ['except' => ['show']]);
+	Route::resource('/item', 'ItemController', ['except' => ['show']]);
+	Route::resource('/role', 'RoleController', ['except' => ['show', 'destroy']]);
+	Route::resource('/user', 'UserController', ['except' => ['show']]);
+	Route::resource('/semakan','kakitangan\semakanController',['except' => ['show','destroy']]);
+	Route::resource('/bantuan','kakitangan\bantuanController',['except' => ['show','destroy']]);
+	Route::resource('/tuntutan','kakitangan\tuntutanController',['except' => ['show','destroy']]);
+	Route::resource('/laporan','kakitangan\laporanController',['except' => ['show','destroy']]);
+	Route::resource('/permohonan-baru','kakitangan\permohonanController',['except' => ['show','destroy']]);
+	Route::group(['prefix' => 'permohonan-baru'], function () {
+		Route::get('/{user_id}', [
+			'uses' => 'kakitangan\permohonanController@show',
+			'as'   => 'permohonan-baru.show',
+		]);
+	});
+
+	Route::get('profile', ['as' => 'profile.edit', 'uses' => 'ProfileController@edit']);
+	Route::get('profile/{link}',function(){
+		return view('profile.index',['link'=>$link]);
+	});
+	Route::put('profile', ['as' => 'profile.update', 'uses' => 'ProfileController@update']);
+	Route::put('profile/password', ['as' => 'profile.password', 'uses' => 'ProfileController@password']);
+
+	Route::get('{page}', ['as' => 'page.index', 'uses' => 'PageController@index']);
+});
+
 
 Route::group(
 	[
