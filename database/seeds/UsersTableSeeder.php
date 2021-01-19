6e93f594dd50906a867042d0225df38fc2e1c951
ainolpipeline <ainol.fhatah@pipeline-network.com>
2021-01-19 10:31:30 +0800

    Added other roles into seed table
    Added other roles into seed table

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -20,65 +20,6 @@ class UsersTableSeeder extends Seeder
         //     'role_id' => 1,
         // ]);
 
-        DB::table('users')->insert([
-            'id' => 2,
-            'name' => 'Penyelia',
-            'email' => 'py@argon.com',
-            'role_id' => 2,
-            'password' => Hash::make('secret'), // secret
-            'remember_token' => Str::random(10),
-            'email_verified_at' => now(),
-            'created_at' => now(),
-            'updated_at' => now()
-        ]);
-
-        DB::table('users')->insert([
-            'id' => 3,
-            'name' => 'Datuk Bandar',
-            'email' => 'db@argon.com',
-            'role_id' => 3,
-            'password' => Hash::make('secret'), // secret
-            'remember_token' => Str::random(10),
-            'email_verified_at' => now(),
-            'created_at' => now(),
-            'updated_at' => now()
-        ]);
-        DB::table('users')->insert([
-            'id' => 4,
-            'name' => 'Ketua Bahagian',
-            'email' => 'kb@argon.com',
-            'role_id' => 4,
-            'password' => Hash::make('secret'), // secret
-            'remember_token' => Str::random(10),
-            'email_verified_at' => now(),
-            'created_at' => now(),
-            'updated_at' => now()
-        ]);
-
-        DB::table('users')->insert([
-            'id' => 5,
-            'name' => 'Ketua Jabatan',
-            'email' => 'kj@argon.com',
-            'role_id' => 5,
-            'password' => Hash::make('secret'), // secret
-            'remember_token' => Str::random(10),
-            'email_verified_at' => now(),
-            'created_at' => now(),
-            'updated_at' => now()
-        ]);
-
-        DB::table('users')->insert([
-            'id' => 8,
-            'name' => 'Kakitangan',
-            'email' => 'kt@argon.com',
-            'role_id' => 8,
-            'password' => Hash::make('secret'), // secret
-            'remember_token' => Str::random(10),
-            'email_verified_at' => now(),
-            'created_at' => now(),
-            'updated_at' => now()
-        ]);
-
     //     factory(App\User::class)->create([
     //         'id' => 2,
     //         'name' => 'Penyelia',
@@ -134,5 +75,102 @@ class UsersTableSeeder extends Seeder
     //         'email' => 'pp@argon.com',
     //         'role_id' => 9,
     //     ]);
+
+
+    DB::table('users')->insert([
+        'id' => 1,
+        'name' => 'Pentadbir Sistem',
+        'email' => 'admin@argon.com',
+        'role_id' => 1,
+        'password' => Hash::make('secret'), // secret
+        'remember_token' => Str::random(10),
+        'email_verified_at' => now(),
+        'created_at' => now(),
+        'updated_at' => now()
+    ]);
+
+    DB::table('users')->insert([
+        'id' => 2,
+        'name' => 'Penyelia',
+        'email' => 'py@argon.com',
+        'role_id' => 2,
+        'password' => Hash::make('secret'), // secret
+        'remember_token' => Str::random(10),
+        'email_verified_at' => now(),
+        'created_at' => now(),
+        'updated_at' => now()
+    ]);
+
+    DB::table('users')->insert([
+        'id' => 3,
+        'name' => 'Datuk Bandar',
+        'email' => 'db@argon.com',
+        'role_id' => 3,
+        'password' => Hash::make('secret'), // secret
+        'remember_token' => Str::random(10),
+        'email_verified_at' => now(),
+        'created_at' => now(),
+        'updated_at' => now()
+    ]);
+    
+    DB::table('users')->insert([
+        'id' => 4,
+        'name' => 'Ketua Bahagian',
+        'email' => 'kb@argon.com',
+        'role_id' => 4,
+        'password' => Hash::make('secret'), // secret
+        'remember_token' => Str::random(10),
+        'email_verified_at' => now(),
+        'created_at' => now(),
+        'updated_at' => now()
+    ]);
+
+    DB::table('users')->insert([
+        'id' => 5,
+        'name' => 'Ketua Jabatan',
+        'email' => 'kj@argon.com',
+        'role_id' => 5,
+        'password' => Hash::make('secret'), // secret
+        'remember_token' => Str::random(10),
+        'email_verified_at' => now(),
+        'created_at' => now(),
+        'updated_at' => now()
+    ]);
+
+    DB::table('users')->insert([
+        'id' => 6,
+        'name' => 'Kerani Semakan',
+        'email' => 'ks@argon.com',
+        'role_id' => 6,
+        'password' => Hash::make('secret'), // secret
+        'remember_token' => Str::random(10),
+        'email_verified_at' => now(),
+        'created_at' => now(),
+        'updated_at' => now()
+    ]);
+
+    DB::table('users')->insert([
+        'id' => 7,
+        'name' => 'Kerani Pemeriksa',
+        'email' => 'kp@argon.com',
+        'role_id' => 7,
+        'password' => Hash::make('secret'), // secret
+        'remember_token' => Str::random(10),
+        'email_verified_at' => now(),
+        'created_at' => now(),
+        'updated_at' => now()
+    ]);
+
+    DB::table('users')->insert([
+        'id' => 8,
+        'name' => 'Kakitangan',
+        'email' => 'kt@argon.com',
+        'role_id' => 8,
+        'password' => Hash::make('secret'), // secret
+        'remember_token' => Str::random(10),
+        'email_verified_at' => now(),
+        'created_at' => now(),
+        'updated_at' => now()
+    ]);
     }
 }
