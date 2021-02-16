cc40ce8cafb1da7be7b568da50c1e734d47cabdd
ainolpipeline <ainol.fhatah@pipeline-network.com>
2021-02-16 12:15:08 +0800

    PermohonanBAru - modified jenisPermohonanlistener to store jenisPermohonanKakitangan one level less than jenis permohonan

--- app/Listeners/UpdateJenisPermohonanListener.php
+++ app/Listeners/UpdateJenisPermohonanListener.php
@@ -33,12 +33,15 @@ class UpdateJenisPermohonanListener
 
         $jenis_permohonan = $event->permohonan->jenis_permohonan;
         $level_permohonan = substr($jenis_permohonan, 0, -1);
+
         $is_individu = $jenis_permohonan[2] == "1" ? 1 : 0;
         $is_approved_peg_sokong = $event->permohonan->peg_sokong_approved == 1 ? 1 : 0;
         $is_NOT_dalam_proses = $event->permohonan->status != 'DALAM PROSES' ? 1 : 0;
 
         $key = array_search($level_permohonan, $array); 
+
         if ($is_approved_peg_sokong && $is_NOT_dalam_proses) {
+            $event->permohonan->jenis_permohonan_kakitangan = $event->permohonan->jenis_permohonan;
             $event->permohonan->jenis_permohonan = $array[$key+1].($is_individu ? 1 : 2);
         }
 
--- app/PermohonanBaru.php
+++ app/PermohonanBaru.php
@@ -26,8 +26,8 @@ class PermohonanBaru extends Model
     protected $attributes = [
         'id_peg_sokong' => 0,
         'id_peg_pelulus' => 0,
-        'jenis_permohonan_kakitangan' => 'DALAM PROSES',
-        'jenis_permohonan' => 'DALAM PROSES'
+        'jenis_permohonan_kakitangan' => 'OT1',
+        'jenis_permohonan' => 'OT1'
     ];
 
     public function scopePermohonanPegawaiSokong($query)
