c71aa4b844e05214bef6ead46ee45f567e42a1ea
ainolpipeline <ainol.fhatah@pipeline-network.com>
2021-02-23 14:39:30 +0800

    Aduan - created controller, model, migration and its Eloquent relation with User

---
+++ app/Aduan.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Aduan extends Model
+{
+    protected $table = 'aduans';
+    protected $primaryKey = 'id_aduan';
+    protected $fillable = [
+        'tajuk',
+        'keterangan'
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'id_user', 'id');
+    }
+
+
+}
