45a91337cb1541924246e27efc1defe2e364046d
ainolpipeline <ainol.fhatah@pipeline-network.com>
2021-03-12 12:21:26 +0800

    Modified jenispermohonanListener to update pivot is_rejected_individually

--- app/permohonan_with_users.php
+++ app/permohonan_with_users.php
@@ -7,12 +7,6 @@ use Illuminate\Database\Eloquent\Model;
 
 class permohonan_with_users extends Model
 {
-    protected static function booted()
-    {
-        static::addGlobalScope('reject', function (Builder $builder) {
-            $builder->where('is_rejected_individually', 1);
-        });
-    }
     protected $table = 'permohonan_with_users';
     protected $fillable = [
         'is_rejected_individually',
