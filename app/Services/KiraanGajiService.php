57c3e2eba074f80c24086892d8f278559bcbde1d
ainolpipeline <ainol.fhatah@pipeline-network.com>
2021-03-15 15:13:44 +0800

    Added services class for calculating elaun

--- app/Services/KiraanGajiService.php
+++ app/Services/KiraanGajiService.php
@@ -2,8 +2,62 @@
 
 namespace App\Services;
 
+use App\PermohonanBaru;
+
 class KiraanGajiService {
-    
+
+    protected const SETAHUN = 12;
+    protected const HARI_BERKERJA = 313;
+    protected const JAM_BERKERJA = 8;
+    protected $permohonan;
+    protected $id_user;
+    protected $jumlahMasaBekerja;
+    protected float $gaji;
+    protected float $kadarPerJam;
+    protected float $jumlahTuntutanRounded;
+
+    /**
+     * Create a new event instance.
+     *
+     * @return void
+     */
+
+    public function __construct(PermohonanBaru $permohonan, $id_user)
+    {
+        $this->id_user = $id_user;
+        $this->permohonan = $permohonan;
+        $this->gaji = $permohonan->gaji;
+        $this->jumlahMasaBekerja = $permohonan->masa;
+        $this->kadarPerJam = $permohonan->kadar_jam;
+        $this->jumlahTuntutanRounded = 0;
+    }
+
+    public function kadarBayaranSejam()
+    {
+        $gajiSetahun = $this->gaji * SETAHUN;
+        (float) $jamBekerja = HARI_BEKERJA * JAM_BEKERJA;
+
+        $kadarBayaranSejam = $gajiSetahun/$jamBekerja;
+
+        return round($kadarBayaranSejam, 2);
+    }
+
+    public function jumlahTuntutan()
+    {
+        $bayaranPerJam = $this->kadarBayaranSejam() * $this->kadarPerJam;
+        $jumlahTuntutan = $bayaranPerJam * $this->jumlahMasaBekerja;
+
+        return $this->jumlahTuntutanRounded = round($jumlahTuntutan, 2);
+    }
+
+    public function jenisTuntutanElaun()
+    {
+        if ($this->jumlahTuntutanRounded >= $this->gaji) {
+            return 'LEBIH SEBULAN';
+        } else {
+            return 'TUNTUTAN BIASA';
+        }
+    }
 }
 
 
