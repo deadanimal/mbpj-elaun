0df3783dd384338bf0c47b15f71ccd4129e85447
ainolpipeline <ainol.fhatah@pipeline-network.com>
2021-04-15 21:59:30 +0800

    fixed bugs

--- app/Http/Controllers/pentadbir_sistem/pengurusanPenggunaController.php
+++ app/Http/Controllers/pentadbir_sistem/pengurusanPenggunaController.php
@@ -8,11 +8,6 @@ use App\User;
 
 class pengurusanPenggunaController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function index()
     {
         $users = User::with('maklumat_pekerjaan')->get();
@@ -25,71 +20,5 @@ class pengurusanPenggunaController extends Controller
         }
 
         return view('core.pentadbir_sistem.pengurusanPengguna');
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        //
     }
 }
