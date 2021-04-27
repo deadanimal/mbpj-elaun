0e7d1ada64ade1fa45e3557b86e45463e27ea70f
ainolpipeline <ainol.fhatah@pipeline-network.com>
2021-04-27 15:55:35 +0800

    fixed bugs

--- resources/views/core/pentadbir_sistem/dashboard.blade.php
+++ resources/views/core/pentadbir_sistem/dashboard.blade.php
@@ -11,9 +11,7 @@
             @endslot
 
             <li class="breadcrumb-item"><a href="{{ route('dashboard.index') }}">{{ __('Dashboards') }}</a></li>
-            {{-- <li class="breadcrumb-item active" aria-current="page">{{ __('Default') }}</li> --}}
         @endcomponent
-        {{-- @include('layouts.headers.cards')  --}}
     @endcomponent
 
     
