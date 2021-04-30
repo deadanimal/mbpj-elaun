151f86f495162f5e724a4fb4ddfb64569a34cdc8
ainolpipeline <ainol.fhatah@pipeline-network.com>
2021-04-30 14:32:25 +0800

    KB, KJ, KP, KS,Pelulus Pindaan - fixed bugs js showUser

--- public/argon/js/penyelia/semakanDatatable.js
+++ public/argon/js/penyelia/semakanDatatable.js
@@ -64,7 +64,6 @@ function showUser() {
                 $("#formOTEL input[name=nama]").val(data.users.NAME);
                 $("#formOTEL input[name=noKPbaru]").val(data.users.NIRC);
                 $("#formOTEL input[name=jabatan]").val(data.users.maklumat_pekerjaan.jabatan.GE_KETERANGAN_JABATAN);
-                $("#formOTEL input[name=jawatan]").val(data.users.maklumat_pekerjaan.jawatan.HR_NAMA_JAWATAN);jabatan.GE_KETERANGAN_JABATAN);
                 $("#formOTEL input[name=jawatan]").val(data.users.maklumat_pekerjaan.jawatan.HR_NAMA_JAWATAN);
 
                 $('input').css('color', 'black')
